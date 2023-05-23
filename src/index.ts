81a11ba7177e496e166bc948aa635e5fc6c51591
svtardust <vstardust@outlook.com>
2023-05-23 22:02:08 +0800

    feat: ✨ 引入热力图刷新按钮
    由于该插件使用的本地缓存，当删除非当日内容块时，所以热力图仍统计该数量，所以引入手动刷新按钮

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,14 @@ export default class CalHeatmap extends Plugin {
     async onload() {
         const cal = document.getElementById("calendarHeatmapBtn");
         CalHeatmap.customAddHtml(cal);
-        const btn = document.getElementById("calendarHeatmapBtn");
-        const con = document.getElementById("openViewHeatmap");
-        this.customEvent(btn, con);
+        const calendarHeatmapBtn = document.getElementById("calendarHeatmapBtn");
+        const openViewHeatmap = document.getElementById("openViewHeatmap");
+        const calendarrefresh = document.getElementById("calendarHeatmapRefresh");
+        this.customEvent(calendarHeatmapBtn, openViewHeatmap, calendarrefresh);
     }
 
     private static customAddHtml(cal: HTMLElement) {
+        // 
         if (cal === null) {
             const barForward = document.getElementById("barForward");
             barForward.insertAdjacentHTML(
@@ -37,36 +39,60 @@ export default class CalHeatmap extends Plugin {
                         overflow: auto;">
                         <div style="width: 1010px; height: 20px;">
                             <div id="customData"
-                            style="width: 600px; padding-left: 65px;
+                            style="width: 600px; padding-left: 65px; float: left; 
+                            height: -webkit-fill-available;
+                            line-height: 45px;
                             font-size: smaller;letter-spacing: 1px;font-weight: lighter;color: #93989f;
                             font-family: -apple-system,sans-serif"Segoe UI","Noto Sans",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";">
                             </div>
+                            <div id="calendarHeatmapRefresh" 
+                            class="toolbar__item b3-tooltips b3-tooltips__sw"
+                            style="float: left; 
+                                    height: 20px; 
+                                    width:25px; 
+                                    margin-left: 280px; 
+                                    line-height: 50px; 
+                                    cursor: pointer;
+                                    padding-top: 4px;"
+                                    aria-label="刷新"
+                                    >
+                                <svg width="20" height="20" t="1684846732026" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3148" width="200" height="200"><path d="M0 0h1024v1024H0z" fill="#FFFFFF" p-id="3149"></path><path d="M295.3 806.3c-7.2 0-14.4-2.4-20.4-7.3-9-7.5-17.8-15.5-26.1-23.8-34.2-34.2-61-74-79.8-118.3-19.4-45.9-29.3-94.7-29.3-144.9s9.8-99 29.3-144.9c18.7-44.3 45.6-84.1 79.8-118.3 34.2-34.2 74-61 118.3-79.8 45.9-19.4 94.7-29.3 144.9-29.3 17.7 0 32 14.3 32 32s-14.3 32-32 32c-82.3 0-159.7 32.1-217.9 90.3-58.2 58.2-90.3 135.6-90.3 217.9 0 82.3 32.1 159.7 90.3 217.9 6.9 6.9 14.2 13.5 21.7 19.7 13.6 11.3 15.5 31.4 4.3 45.1-6.4 7.7-15.6 11.7-24.8 11.7zM512 884.2c-17.7 0-32-14.3-32-32s14.3-32 32-32c82.3 0 159.7-32.1 217.9-90.3s90.3-135.6 90.3-217.9-32.1-159.7-90.3-217.9c-7.4-7.4-15.2-14.5-23.3-21.1-13.7-11.2-15.7-31.3-4.6-45 11.2-13.7 31.3-15.7 45-4.6 9.8 8 19.2 16.5 28.1 25.4 34.2 34.2 61 74 79.8 118.3 19.4 45.9 29.3 94.7 29.3 144.9s-9.8 99-29.3 144.9c-18.7 44.3-45.6 84.1-79.8 118.3-34.2 34.2-74 61-118.3 79.8-45.8 19.3-94.6 29.2-144.8 29.2z" fill="#B3B3B3" p-id="3150"></path><path d="M518.8 70.8l98.3 98.3c1.6 1.6 1.6 4.1 0 5.7l-98.3 98.3c-2.5 2.5-6.8 0.7-6.8-2.8V73.7c0-3.6 4.3-5.4 6.8-2.9zM505.2 751.2l-98.3 98.3c-1.6 1.6-1.6 4.1 0 5.7l98.3 98.3c2.5 2.5 6.8 0.7 6.8-2.8V754.1c0-3.6-4.3-5.4-6.8-2.9z" fill="#B3B3B3" p-id="3151"></path></svg>
+                            </div>
                         </div>
-                        <div id="calendarHeatmapContent"></div>
+                        <div id="calendarHeatmapContent">
+                        </div>
                     </div>
                 </div>`
             );
         }
     }
 
-    private customEvent(btn, con) {
-        btn.addEventListener(
+    private customEvent(calendarHeatmapBtn, openViewHeatmap, calendarrefresh) {
+        calendarrefresh.addEventListener("click", async (event) => {
+            event.stopPropagation();
+            localStorage.removeItem("calendar-heatmap-data");
+            await this.heatmap();
+
+        })
+        calendarHeatmapBtn.addEventListener(
             "click",
             async (event) => {
                 event.stopPropagation();
-                if (con.style.visibility === "hidden") {
-                    con.style.visibility = "visible";
+                if (openViewHeatmap.style.visibility === "hidden") {
+                    openViewHeatmap.style.visibility = "visible";
                     await this.heatmap();
                 } else {
-                    con.style.visibility = "hidden";
+                    openViewHeatmap.style.visibility = "hidden";
                 }
             }, false);
         window.addEventListener(
             "click",
             (event) => {
-                event.stopPropagation();
-                if (con.style.visibility === "visible") {
-                    con.style.visibility = "hidden";
+                if (event.clientX <= 50 || event.clientX >= 1048 || event.clientY >= 235) {
+                    event.stopPropagation();
+                    if (openViewHeatmap.style.visibility === "visible") {
+                        openViewHeatmap.style.visibility = "hidden";
+                    }
                 }
             }, false);
     }
