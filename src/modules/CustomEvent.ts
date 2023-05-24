5fdcbfa8d6e578ba12f90013d02fd9ca1f2696d7
svtardust <vstardust@outlook.com>
2023-05-24 21:21:38 +0800

    refactor🎨: 结构调整

--- src/modules/CustomEvent.ts
+++ src/modules/CustomEvent.ts
@@ -1,35 +1,34 @@
 import { heatmap } from "./Heatmap";
 
-export function customEvent(){
-  const calendarHeatmapBtn = document.getElementById("calendarHeatmapBtn");
-  const openViewHeatmap = document.getElementById("openViewHeatmap");
-  const calendarrefresh = document.getElementById("calendarHeatmapRefresh");
-  
-  calendarrefresh.addEventListener("click", async (event) => {
-      event.stopPropagation();
-      localStorage.removeItem("calendar-heatmap-data");
-      await heatmap();
+export function customEvent() {
+    const openViewHeatmap = document.getElementById("openViewHeatmap");
 
-  })
-  calendarHeatmapBtn.addEventListener(
-      "click",
-      async (event) => {
-          event.stopPropagation();
-          if (openViewHeatmap.style.visibility === "hidden") {
-              openViewHeatmap.style.visibility = "visible";
-              await heatmap();
-          } else {
-              openViewHeatmap.style.visibility = "hidden";
-          }
-      }, false);
-  window.addEventListener(
-      "click",
-      (event) => {
-          if (event.clientX <= 50 || event.clientX >= 1048 || event.clientY >= 235) {
-              event.stopPropagation();
-              if (openViewHeatmap.style.visibility === "visible") {
-                  openViewHeatmap.style.visibility = "hidden";
-              }
-          }
-      }, false);
+    async function calendarrefresh(event) {
+        event.stopPropagation();
+        localStorage.removeItem("calendar-heatmap-data");
+        await heatmap();
+    }
+
+    async function eventHeatmapButton(event) {
+        event.stopPropagation();
+        if (openViewHeatmap.style.visibility === "hidden") {
+            openViewHeatmap.style.visibility = "visible";
+            await heatmap();
+        } else {
+            openViewHeatmap.style.visibility = "hidden";
+        }
+    }
+
+    function windowRadiusClose(event) {
+        if (event.clientX <= 50 || event.clientX >= 1048 || event.clientY >= 235) {
+            event.stopPropagation();
+            if (openViewHeatmap.style.visibility === "visible") {
+                openViewHeatmap.style.visibility = "hidden";
+            }
+        }
+    }
+    document.getElementById("calendarHeatmapRefresh").addEventListener("click", calendarrefresh);
+    document.getElementById("calendarHeatmapBtn").addEventListener("click", eventHeatmapButton);
+
+    window.addEventListener("click", windowRadiusClose);
 }
