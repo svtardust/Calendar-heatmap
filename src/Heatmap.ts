7bcf0354aa94e49b6c1a0f1e9127363f6c6ec854
svtardust <132273422+svtardust@users.noreply.github.com>
2023-09-03 17:55:53 +0800

    🐞 fix: 日历热力图月份显示混乱
    日历热力图月份显示混乱，有重复和缺失月份  Fixes #40
    
    https://githubfast.com/svtardust/Calendar-heatmap/issues/40

--- src/Heatmap.ts
+++ src/Heatmap.ts
@@ -1,11 +1,13 @@
 import * as d3 from 'd3'
 import axios from 'axios'
-import {getColor, getData} from "./utils";
+import { getColor, getData } from "./utils";
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn'
 
 export async function heatmap() {
   const width = 815
   const height = 180
-  const margin = {top: 15, right: 30, bottom: 30, left: 25}
+  const margin = { top: 15, right: 30, bottom: 30, left: 25 }
   const weekBoxWidth = 20
   const monthBoxHeight = 20
 
@@ -14,7 +16,7 @@ export async function heatmap() {
   // 获取svg并定义svg高度和宽度
   const svg = d3.select('#calendarHeatmapContent').append('svg').attr('width', width).attr('height', height - 55)
   // 绘制图区
-  const {months, days} = await dataChart()
+  const { months, days } = await dataChart()
   monthCoordinate(width, margin, weekBoxWidth, svg, months)
   weekCoordinate(height, margin, monthBoxHeight, svg)
   await dateSquares(height, margin, weekBoxWidth, monthBoxHeight, svg, days)
@@ -151,12 +153,12 @@ async function dateSquares(height, margin, weekBoxWidth, monthBoxHeight, svg, da
     })
     // 日期方块添加鼠标移入时的数据提示
     .append('title').text((d) => {
-    let message = '没有内容块'
-    if (d.total != 0 || d.total != undefined) {
-      message = '有' + d.total + '个内容块'
-    }
-    return d.date + '\n' + message
-  })
+      let message = '没有内容块'
+      if (d.total != 0 || d.total != undefined) {
+        message = '有' + d.total + '个内容块'
+      }
+      return d.date + '\n' + message
+    })
   // 如果是当日日期边框显示为红色
   const date = new Date()
   const day = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
@@ -164,11 +166,11 @@ async function dateSquares(height, margin, weekBoxWidth, monthBoxHeight, svg, da
 }
 
 async function queryDate() {
-  const {isdailyNote, ignoreText} = await getData()
+  const { isdailyNote, ignoreText } = await getData()
   let response
   if (isdailyNote === true) {
     const sql = `SELECT SUBSTR(created, 1, 8) AS date, COUNT(*) count FROM blocks WHERE type = 'p' AND  hpath LIKE '/daily note%' GROUP BY SUBSTR(created, 1, 8) ORDER BY date DESC LIMIT 370`
-    response = await (await axios.post('/api/query/sql', {stmt: sql})).data.data
+    response = await (await axios.post('/api/query/sql', { stmt: sql })).data.data
   } else if (ignoreText !== null && ignoreText != '') {
     let sql = `SELECT SUBSTR(created, 1, 8) AS date, COUNT(*) count FROM blocks WHERE type = 'p' AND `
     const arrData = ignoreText.split(',')
@@ -180,10 +182,10 @@ async function queryDate() {
       }
     }
     sql = sql + `GROUP BY SUBSTR(created, 1, 8) ORDER BY date DESC LIMIT 370 `
-    response = await (await axios.post('/api/query/sql', {stmt: sql})).data.data
+    response = await (await axios.post('/api/query/sql', { stmt: sql })).data.data
   } else {
     const sql = `SELECT SUBSTR(created, 1, 8) AS date, COUNT(*) count FROM blocks WHERE type = 'p' GROUP BY SUBSTR(created, 1, 8) ORDER BY date DESC LIMIT 370`
-    response = await (await axios.post('/api/query/sql', {stmt: sql})).data.data
+    response = await (await axios.post('/api/query/sql', { stmt: sql })).data.data
   }
   return response
 }
@@ -194,10 +196,10 @@ async function dataChart() {
   // 遍历数据
   if (resDate != null) {
     resDate.forEach(param => {
-      const {date, count} = param
+      const { date, count } = param
       // 格式化date，封装进新数组
       const formatDate = `${date.substring(0, 4)}-${(date.substring(4, 6) > 10 ? date.substring(4, 6) : date.substring(4, 6).substring(1, 2))}-${(date.substring(6, 8) > 10 ? date.substring(6, 8) : date.substring(6, 8).substring(1.2))}`
-      formatParams.push({day: formatDate, total: count})
+      formatParams.push({ day: formatDate, total: count })
     })
   }
 
@@ -205,24 +207,37 @@ async function dataChart() {
   const days = []
 
   for (let i = 12; i > 0; i--) {
-    const referDate = new Date()
-
-    referDate.setMonth(referDate.getMonth() - i + 2)
-    referDate.setDate(0)
-
-    let month: string | number = referDate.getMonth() + 1
-
-    for (let j = 1; j <= referDate.getDate(); j++) {
-      let data = {date: referDate.getFullYear() + '-' + month + '-' + j, total: 0}
+    const referDate = dayjs(new Date())
+    const referDay = referDate.month((referDate.month() - i + 1))
+    for (let j = 1; j <= referDay.daysInMonth(); j++) {
+      let data = { date: referDay.year() + '-' + (referDay.month() + 1) + '-' + j, total: 0 }
       formatParams.forEach(item => {
         if (item.day === data.date) {
           data.total = item.total
         }
       })
-
       days.push(data)
     }
-    months.push(referDate.getFullYear() + '-' + month)
+    months.push(referDay.year() + '-' + (referDay.month() + 1))
+
+
+    // referDate.setMonth(referDate.getMonth() - i + 2)
+    // referDate.setDate(0)
+    // console.log(referDate.getDate());
+
+    // let month: string | number = referDate.getMonth() + 1
+
+    // for (let j = 1; j <= referDate.getDate(); j++) {
+    //   let data = {date: referDate.getFullYear() + '-' + month + '-' + j, total: 0}
+    //   formatParams.forEach(item => {
+    //     if (item.day === data.date) {
+    //       data.total = item.total
+    //     }
+    //   })
+
+    //   days.push(data)
+    // }
+    // months.push(referDate.getFullYear() + '-' + month)
   }
   let firstDate = days[0].date
 
@@ -251,8 +266,8 @@ async function dataChart() {
         total = item.total
       }
     })
-    days.unshift({date: formatDate.join('-'), total})
+    days.unshift({ date: formatDate.join('-'), total })
   }
-  return {days, months}
+  return { days, months }
 }
 
