bb8ed41df2b279a6ae55705fb30961c7ad5b155c
vstardust <vstardust@outlook.com>
2025-01-21 20:50:45 +0800

    🦄 refactor(scripts): 构建脚本修改

--- scripts/make_dev_link.js
+++ scripts/make_dev_link.js
@@ -1,117 +1,66 @@
+/*
+ * Copyright (c) 2024 by frostime. All Rights Reserved.
+ * @Author       : frostime
+ * @Date         : 2023-07-15 15:31:31
+ * @FilePath     : /scripts/make_dev_link.js
+ * @LastEditTime : 2024-09-06 18:13:53
+ * @Description  : 
+ */
+// make_dev_link.js
 import fs from 'fs';
-import readline  from 'node:readline';
+import { log, error, getSiYuanDir, chooseTarget, getThisPluginName, makeSymbolicLink } from './utils.js';
 
-
-//************************************ Write you dir here ************************************
-
-//Please write the "workspace/data/plugins" directory here
-//请在这里填写你的 "workspace/data/plugins" 目录
 let targetDir = '';
-//Like this
-// const targetDir = `H:\\SiYuanDevSpace\\data\\plugins`;
-//********************************************************************************************
 
-const log = console.log;
-
-async function getSiYuanDir() {
-    let url = 'http://127.0.0.1:6806/api/system/getWorkspaces';
-    let header = {
-        // "Authorization": `Token ${token}`,
-        "Content-Type": "application/json",
-    }
-    let conf = {};
-    try {
-        let response = await fetch(url, {
-            method: 'POST',
-            headers: header
-        });
-        if (response.ok) {
-            conf = await response.json();
-        } else {
-            log(`HTTP-Error: ${response.status}`);
-            return null;
-        }
-    } catch (e) {
-        log("Error:", e);
-        log("Please make sure SiYuan is running!!!");
-        return null;
-    }
-    return conf.data;
-}
-
-async function chooseTarget(workspaces) {
-    let count = workspaces.length;
-    log(`Got ${count} SiYuan ${count > 1 ? 'workspaces' : 'workspace'}`)
-    for (let i = 0; i < workspaces.length; i++) {
-        log(`[${i}] ${workspaces[i].path}`);
-    }
-
-    if (count == 1) {
-        return `${workspaces[0].path}/data/plugins`;
-    } else {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        let index = await new Promise((resolve, reject) => {
-            rl.question(`Please select a workspace[0-${count-1}]: `, (answer) => {
-                resolve(answer);
-            });
-        });
-        rl.close();
-        return `${workspaces[index].path}/data/plugins`;
-    }
-}
-
+/**
+ * 1. Get the parent directory to install the plugin
+ */
+log('>>> Try to visit constant "targetDir" in make_dev_link.js...');
 if (targetDir === '') {
-    log('"targetDir" is empty, try to get SiYuan directory automatically....')
+    log('>>> Constant "targetDir" is empty, try to get SiYuan directory automatically....');
     let res = await getSiYuanDir();
-    
-    if (res === null) {
-        log('Failed! You can set the plugin directory in scripts/make_dev_link.js and try again');
-        process.exit(1);
+
+    if (!res || res.length === 0) {
+        log('>>> Can not get SiYuan directory automatically, try to visit environment variable "SIYUAN_PLUGIN_DIR"....');
+        let env = process.env?.SIYUAN_PLUGIN_DIR;
+        if (env) {
+            targetDir = env;
+            log(`\tGot target directory from environment variable "SIYUAN_PLUGIN_DIR": ${targetDir}`);
+        } else {
+            error('\tCan not get SiYuan directory from environment variable "SIYUAN_PLUGIN_DIR", failed!');
+            process.exit(1);
+        }
+    } else {
+        targetDir = await chooseTarget(res);
     }
 
-    targetDir = await chooseTarget(res);
-    log(`Got target directory: ${targetDir}`);
+    log(`>>> Successfully got target directory: ${targetDir}`);
 }
-
-//Check
 if (!fs.existsSync(targetDir)) {
-    log(`Failed! plugin directory not exists: "${targetDir}"`);
-    log(`Please set the plugin directory in scripts/make_dev_link.js`);
+    error(`Failed! Plugin directory not exists: "${targetDir}"`);
+    error('Please set the plugin directory in scripts/make_dev_link.js');
     process.exit(1);
 }
 
-
-//check if plugin.json exists
-if (!fs.existsSync('./plugin.json')) {
-    console.error('Failed! plugin.json not found');
-    process.exit(1);
-}
-
-//load plugin.json
-const plugin = JSON.parse(fs.readFileSync('./plugin.json', 'utf8'));
-const name = plugin?.name;
-if (!name || name === '') {
-    log('Failed! Please set plugin name in plugin.json');
-    process.exit(1);
-}
-
-//dev directory
-const devDir = `./dev`;
-//mkdir if not exists
+/**
+ * 2. The dev directory, which contains the compiled plugin code
+ */
+const devDir = `${process.cwd()}/dev`;
 if (!fs.existsSync(devDir)) {
     fs.mkdirSync(devDir);
 }
 
-const targetPath = `${targetDir}/${name}`;
-//如果已经存在，就退出
-if (fs.existsSync(targetPath)) {
-    log(`Failed! Target directory  ${targetPath} already exists`);
-} else {
-    //创建软链接
-    fs.symlinkSync(`${process.cwd()}/dev`, targetPath, 'junction');
-    log(`Done! Created symlink ${targetPath}`);
-}
 
+/**
+ * 3. The target directory to make symbolic link to dev directory
+ */
+const name = getThisPluginName();
+if (name === null) {
+    process.exit(1);
+}
+const targetPath = `${targetDir}/${name}`;
+
+/**
+ * 4. Make symbolic link
+ */
+makeSymbolicLink(devDir, targetPath);
